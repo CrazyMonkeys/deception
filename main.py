6f10a340db351c5096d9e1353447aa4b3104e38a
unknown <vbaron@NCEL2579.iis.amadeus.net>
2015-10-15 10:35:32 +0200

    merge

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import math
 
 
-
 class board:
     def __init__(self):
         self.xMax = 30
@@ -18,23 +17,20 @@ class board:
 class move:
     def __init__(self, iEnumMove):
         self.value = iEnumMove
-        pass
-
 
 class game:
     def __init__(self):
         self.board = board()
         self.playerPosition = []
         self.playerDronePosition = []
-        pass
 
     def refreshPosition(iSelf, iX, iY):
-        pass
+        iSelf.playerPosition = [iX, iY]
 
-    def refreshRemovePosition(iSelf, iX, iY)
-        pass
+    def refreshRemovePosition(iSelf, iX, iY):
+        iSelf.playerDronePosition[iX, iY]
 
-    def applyRefresh():
+    def applyRefresh(iSelf):
         pass
 
     def applyMove(self, iMove):
@@ -95,18 +91,23 @@ player_count = int(raw_input())
 my_id = int(raw_input())
 
 # game loop
+
+myGame = game()
+
 while 1:
     helper_bots = int(raw_input())
     for i in xrange(player_count):
         x, y = [int(j) for j in raw_input().split()]
+        game.refreshPosition(x, y)
     removal_count = int(raw_input())
     for i in xrange(removal_count):
         remove_x, remove_y = [int(j) for j in raw_input().split()]
+        game.refreshRemovePosition(remove_x, remove_y)
 
+    game.applyRefresh()
 
-
+    theWinMove = miniMax.miniMax(myGame)
+    print theWinMove.getvalue()
 
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
-
-    print "DOWN"
