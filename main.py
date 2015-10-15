b330eae150e692e4923de15bb7dbb91279b1b2b1
unknown <spalmier@NCEL3027.iis.amadeus.net>
2015-10-15 08:49:53 +0200

    Adding file

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import math
+import re
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+global LOGGING
+LOGGING = False
+
+def cprint(iString):
+    if LOGGING:
+        print iString
