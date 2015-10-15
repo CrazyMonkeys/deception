c653218aee5c357e798fa43e3382ffc377285012
unknown <vbaron@NCEL2579.iis.amadeus.net>
2015-10-15 10:52:49 +0200

    merge

--- main.py
+++ main.py
@@ -61,15 +61,20 @@ class game:
         self.board = board()
         self.playerPosition = []
         self.playerDronePosition = []
+        self.myPosition = [-1, -1]
 
-    def refreshPosition(iSelf, iX, iY):
-        iSelf.playerPosition = [iX, iY]
+    def refreshPosition(self, iX, iY):
+        self.playerPosition.append([iX, iY])
 
-    def refreshRemovePosition(iSelf, iX, iY):
-        iSelf.playerDronePosition[iX, iY]
+    def setMyPosition(self, x ,y):
+        self.myPosition = [x, y]
 
-    def applyRefresh(iSelf):
-        pass
+    def refreshRemovePosition(self, iX, iY):
+        self.playerDronePosition[iX, iY]
+
+    def applyRefresh(self):
+        for position in self.playerPosition = []:
+            self.board()
 
     def applyMove(self, iMove):
         # return a copy a the current game after the move is applied
@@ -136,7 +141,10 @@ while 1:
     helper_bots = int(raw_input())
     for i in xrange(player_count):
         x, y = [int(j) for j in raw_input().split()]
-        myGame.refreshPosition(x, y)
+        if i == my_id:
+            myGame.setMyPosition(x, y)
+        else:
+            myGame.refreshPosition(x, y)
     removal_count = int(raw_input())
     for i in xrange(removal_count):
         remove_x, remove_y = [int(j) for j in raw_input().split()]
